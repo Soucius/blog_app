3a46a5d3ae847cce13db2d9839b8f97ad0149a59
Soucius <soucius.ggwp@gmail.com>
2024-08-24 01:32:40 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include "partials/header.php";
+    include "options/database.php";
+    include "partials/nav.php";
+?>
+    <div class="container mt-5">
+        <div class="card mx-auto">
+            <div class="card-header text-center bg-primary text-white">
+                <h2 class="card-title">Blogs</h2>
+            </div>
+    
+            <div class="card-body">
+                <div class="container">
+                    
+                </div>
+            </div>
+    
+            <div class="card-footer">
+                <span>Total blogs: </span>
+            </div>
+        </div>
+    </div>
+
+<?php include "partials/footer.php"; ?>
